d4e759d747278b405dab54ba522f7f82d89edb45
Jorge Mg <44137164+itsjorgemg@users.noreply.github.com>
2018-11-22 22:53:11 +0100

    Comportamenti

---
+++ Assets/Scripts/Behaviours/AgentBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour {
+    public GameObject target;
+    protected Agent agent;
+
+    public virtual void Awake() {
+        agent = gameObject.GetComponent<Agent>();
+    }
+
+    public virtual void Update() {
+        agent.SetSteering(GetSteering());
+    }
+
+    public virtual Steering GetSteering() {
+        return new Steering();
+    }
+
+}
---
+++ Assets/Scripts/Behaviours/BackForth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackForth : Seek {
+    [SerializeField] public Vector3 distance;
+
+    private Vector3 startPoint, endPoint;
+
+    public override void Awake() {
+        base.Awake();
+        startPoint = transform.position;
+        endPoint = startPoint + distance;
+        target = new GameObject();
+        target.transform.position = endPoint;
+    }
+
+    public override Steering GetSteering() {
+        if (Vector3.Distance(transform.position, target.transform.position) < 0.1f) {
+            target.transform.position = target.transform.position == startPoint ? endPoint : startPoint;
+        }
+        return base.GetSteering();
+    }
+
+    void OnDestroy() {
+        Destroy(target);
+    }
+}
